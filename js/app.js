fad1173f3995b5a9b9e9ddfe04fd2750dede4e1b
SheaClose <SheaClose@gmail.com>
2017-09-18 09:07:00 -0500

    done

--- js/app.js
+++ js/app.js
@@ -1,17 +1,16 @@
 angular
   .module('app', ['ui.router'])
   .config(($stateProvider, $urlRouterProvider) => {
-    /* Set home route */
     $urlRouterProvider.otherwise('/');
     $stateProvider
       .state('home', {
         url: '/',
-        templateUrl: `../views/home.html`,
+        templateUrl: `/views/home.html`,
         controller: 'mainCtrl'
       })
       .state('about', {
         url: '/about',
-        templateUrl: `../views/about.html`,
+        templateUrl: `/views/about.html`,
         controller: 'secondaryCtrl'
       });
   });
